d12417701e7910daa8c44c44ddd72ea0c9fa5f38
JT Cho <jonathan.t.cho@gmail.com>
2016-03-09 11:04:42 -0500

    added socket.io bindings

--- voting-server/src/server.js
+++ voting-server/src/server.js
@@ -2,6 +2,22 @@
 
 import Server from 'socket.io';
 
-export default function startServer() {
+export default function startServer(store) {
+    //Initialize the Socket.IO server.
     const io = new Server().attach(8090);
+
+    //Bind a listener called whenever an action is dispatched by
+    //the store.
+    store.subscribe(
+        //Emits the *whole* state to everyone connected whenever
+        //changes occur. This can be expensive, so consider
+        //only emitting relevant subset/diffs instead of snapshots...
+        () => io.emit('state', store.getState().toJS())
+    );
+
+    //Emit current state immediately on client connection.
+    io.on('connection', (socket) => {
+        socket.emit('state', store.getState().toJS());
+        socket.on('action', store.dispatch.bind(store));
+    });
 }
